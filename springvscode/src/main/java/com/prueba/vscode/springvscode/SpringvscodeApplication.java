6b84c34496e3dbc78c9b85876db0650930f58357
mcalvachejose <ladridosdelperromagico97@gmail.com>
2025-05-28 15:27:32 +0200

    [HM] Add .proeprties files to main class

--- springvscode/src/main/java/com/prueba/vscode/springvscode/SpringvscodeApplication.java
+++ springvscode/src/main/java/com/prueba/vscode/springvscode/SpringvscodeApplication.java
@@ -2,8 +2,13 @@ package com.prueba.vscode.springvscode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources({
+	@PropertySource("classpath:values.properties"),
+})
 public class SpringvscodeApplication {
 
 	public static void main(String[] args) {
