1ff51197c062fea0163f27a0a6c615ab650e37b5
mcalvachejose <ladridosdelperromagico97@gmail.com>
2025-05-30 10:10:07 +0200

    [HM] @Values with operations, Enviroment parse data

--- springvscode/src/main/java/com/prueba/vscode/springvscode/SpringvscodeApplication.java
+++ springvscode/src/main/java/com/prueba/vscode/springvscode/SpringvscodeApplication.java
@@ -2,13 +2,8 @@ package com.prueba.vscode.springvscode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
-@PropertySources({
-	@PropertySource("classpath:values.properties"),
-})
 public class SpringvscodeApplication {
 
 	public static void main(String[] args) {
--- springvscode/src/main/java/com/prueba/vscode/springvscode/controller/PathVariableController.java
+++ springvscode/src/main/java/com/prueba/vscode/springvscode/controller/PathVariableController.java
@@ -1,9 +1,12 @@
 package com.prueba.vscode.springvscode.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,6 +36,27 @@ public class PathVariableController {
     @Value("${prueba.palabra}")
     private String prueba;
 
+    @Value("${config.listValues}")
+    private List<String> listValues;
+
+    @Value("#{ '${config.listValues}'.toUpperCase().split('A') }")
+    private List<String> listaString;
+
+    @Value("#{ '${config.listValues}'.toUpperCase() }")
+    private String stringLista;
+
+    @Value("#{${configuracion.valuesMap}}")
+    private Map<String, Object> valuesMap;
+
+    @Value("#{${configuracion.valuesMap}.color}")
+    private String color;
+
+    @Value("#{${configuracion.valuesMap}.product}")
+    private String product;
+
+    @Autowired
+    private Environment env;
+
     @GetMapping("/baz/{message}")
     public ParamDto baz(@PathVariable(name="message") String messageOtro) {
         ParamDto param = new ParamDto();
@@ -61,9 +85,18 @@ public class PathVariableController {
         Map<String, Object> values = new HashMap<>();
         values.put("username", username);
         values.put("message", message);
+        values.put("message2", env.getProperty("config.message"));
         values.put("code", code);
+        values.put("code2", Integer.valueOf(env.getProperty("config.code")));
+        values.put("code3", env.getProperty("config.code", Long.class));
         values.put("listOfValues", listOfValues);
         values.put("prueba", prueba);
+        values.put("listValues", listValues);
+        values.put("listaString", listaString);
+        values.put("stringLista", stringLista);
+        values.put("valuesMap", valuesMap);
+        values.put("color", color);
+        values.put("product", product);
         return values;
     }
 }
