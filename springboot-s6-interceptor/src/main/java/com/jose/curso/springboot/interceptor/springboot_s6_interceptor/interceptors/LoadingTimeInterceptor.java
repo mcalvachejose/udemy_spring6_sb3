4f16746f9597b044a70f3952c78bde9714776798
mcalvachejose <ladridosdelperromagico97@gmail.com>
2025-06-17 10:18:02 +0200

    [HM] S6 Inyect Interceptor, preHandle postHandle

--- springboot-s6-interceptor/src/main/java/com/jose/curso/springboot/interceptor/springboot_s6_interceptor/interceptors/LoadingTimeInterceptor.java
+++ springboot-s6-interceptor/src/main/java/com/jose/curso/springboot/interceptor/springboot_s6_interceptor/interceptors/LoadingTimeInterceptor.java
@@ -1,25 +1,32 @@
 package com.jose.curso.springboot.interceptor.springboot_s6_interceptor.interceptors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@Component
+@Component("timeInterceptor")
 public class LoadingTimeInterceptor implements HandlerInterceptor{
+
+    private static final Logger log = LoggerFactory.getLogger(LoadingTimeInterceptor.class);
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
             @Nullable ModelAndView modelAndView) throws Exception {
-        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+        HandlerMethod controller = (HandlerMethod)handler;
+        log.info("LoadingTimeInterceptor : postHandle() saliendo...." + controller.getMethod().getName());
     }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
+                log.info("LoadingTimeInterceptor : preHandle() entrado...." + ((HandlerMethod)handler).getMethod().getName());
         return true;
     }
     
