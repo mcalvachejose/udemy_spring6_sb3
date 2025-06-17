4cf3559d92439d49bca45d8d07bc579e13ee6a7c
mcalvachejose <ladridosdelperromagico97@gmail.com>
2025-06-17 11:44:40 +0200

    [HM] S6 Custome response when interceptor return false, mapper json

--- springboot-s6-interceptor/src/main/java/com/jose/curso/springboot/interceptor/springboot_s6_interceptor/interceptors/LoadingTimeInterceptor.java
+++ springboot-s6-interceptor/src/main/java/com/jose/curso/springboot/interceptor/springboot_s6_interceptor/interceptors/LoadingTimeInterceptor.java
@@ -1,5 +1,8 @@
 package com.jose.curso.springboot.interceptor.springboot_s6_interceptor.interceptors;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 import org.slf4j.Logger;
@@ -9,6 +12,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -28,7 +33,19 @@ public class LoadingTimeInterceptor implements HandlerInterceptor{
         Random random = new Random();
         int delay = random.nextInt(500);
         Thread.sleep(delay);
-        return true;
+
+        Map<String, String> json = new HashMap<>();
+        json.put("error", "no tienes acceso a la pagina");
+        json.put("date", new Date().toString());
+
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = mapper.writeValueAsString(json);
+        response.setContentType("application/json");
+        response.setStatus(401);
+        response.getWriter().write(jsonString);
+        
+        return false;
+        //return true;
     }
     
     @Override
